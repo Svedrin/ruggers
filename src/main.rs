5e54399f1fe237b9629337c3bbc2a33ef29887fe
Michael Ziegler <michael.ziegler@proemion.com>
2018-04-03 21:01:10 +0200

    greatly reduce the suckiness by using Enums

--- src/main.rs
+++ src/main.rs
@@ -12,26 +12,21 @@ mod ruggers;
 use ruggers::{RuggedRecord, RuggedGeneration};
 
 #[derive(Serialize, Deserialize)]
-struct CmdPacket {
-    cmd: String,
-    #[serde(default)]
-    key: String,
-    #[serde(default)]
-    val: String,
-    #[serde(default)]
-    gen: u64
+enum Command {
+    Get   ( String ),
+    Set   ( String, String ),
+    Merge ( String, String, u64 ),
 }
 
 #[derive(Serialize, Deserialize)]
-struct CmdResult {
-    res: String,
-    key: String,
-    val: String,
-    gen: u64
+enum CmdResult {
+    Ok,
+    Error ( String ),
+    Value ( String, String ),
 }
 
 fn main() {
-    let mut gen = RuggedGeneration::new_root(1);
+    let mut datastore = RuggedGeneration::new_root(1);
 
     let socket = UdpSocket::bind("[::]:22422").unwrap();
 
@@ -41,79 +36,44 @@ fn main() {
         let (len, src) = socket.recv_from(&mut buf)
             .expect("Couldn't read from socket");
 
-        let resp = match serde_json::from_str::<CmdPacket>(std::str::from_utf8(&buf[..len])
+        let resp = match serde_json::from_str::<Command>(std::str::from_utf8(&buf[..len])
             .expect("send me utf8 please"))
         {
             Ok(cmd) => {
-                if cmd.cmd == String::from("get") {
-                    if let Some(val) = gen.get(&cmd.key) {
-                        CmdResult {
-                            res: String::from("get"),
-                            key: cmd.key,
-                            val: val.value.to_owned(),
-                            gen: 0,
-                        }
-                    } else {
-                        CmdResult {
-                            res: String::from("get"),
-                            key: cmd.key,
-                            val: String::from(""),
-                            gen: 0,
+                match cmd {
+                    Command::Get(key) => {
+                        if let Some(val) = datastore.get(&key) {
+                            CmdResult::Value(key, val.as_ref().value.to_owned())
+                        } else {
+                            CmdResult::Value(key, String::from(""))
                         }
                     }
-                }
-                else if cmd.cmd == String::from("mrg") {
-                    match gen.merge(RuggedRecord::new(cmd.gen, 0, cmd.key, cmd.val)) {
-                        Some(new_gen) => {
-                            gen = new_gen;
-                            CmdResult {
-                                res: String::from("ack"),
-                                key: String::from(""),
-                                val: String::from(""),
-                                gen: gen.this_gen(),
+                    Command::Set(key, val) => {
+                        datastore = datastore.store(&key, &val);
+                        CmdResult::Ok
+                    }
+                    Command::Merge(key, val, gen) => {
+                        match datastore.merge(RuggedRecord::new(gen, 0, key, val)) {
+                            Some(new_gen) => {
+                                datastore = new_gen;
+                                CmdResult::Ok
+                            }
+                            None => {
+                                CmdResult::Error(String::from("Merge conflict"))
                             }
                         }
-                        None => {
-                            CmdResult {
-                                res: String::from("nak"),
-                                key: String::from(""),
-                                val: String::from(""),
-                                gen: 0,
-                            }
-                        }
-                    }
-                }
-                else if cmd.cmd == String::from("set") {
-                    gen = gen.store(&cmd.key, &cmd.val);
-                    CmdResult {
-                        res: String::from("set"),
-                        key: cmd.key,
-                        val: String::from(""),
-                        gen: 0,
-                    }
-                }
-                else {
-                    CmdResult {
-                        res: String::from("nak"),
-                        key: String::from(""),
-                        val: String::from(""),
-                        gen: 0,
                     }
                 }
             }
             Err(err) => {
-                CmdResult {
-                    res: String::from("err"),
-                    key: String::from(""),
-                    val: err.to_string(),
-                    gen: 0,
-                }
+                CmdResult::Error(err.to_string())
             }
         };
 
         let mut ser_data = serde_json::to_string(&resp)
             .expect("Couldn't encode response");
-        ser_data.write_str("\n");
+        ser_data.write_str("\n")
+            .expect("Can't append newline to data :s");
         socket.send_to(&ser_data[..].as_bytes(), &src)
             .expect("Couldn't send");
     }
